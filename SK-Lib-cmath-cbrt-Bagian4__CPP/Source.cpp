87b1d36c5a79b3cdc58a3ce6c6246bd9f34b8a1b
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-10 08:14:06 +0700

    Add project files.

---
+++ SK-Lib-cmath-cbrt-Bagian4__CPP/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    long x = 964353422;
+    double hasil = cbrt(x);
+    cout << "Akar kubik dari " << x << " adalah " << hasil << endl;
+    _getch();
+    return 0;
+}
